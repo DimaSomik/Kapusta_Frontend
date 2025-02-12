5e3b6960cf5b32e55382ca6a31afc14ee651ca65
Grzesiek1982 <grzesieg1982@interia.pl>
2025-02-12 02:28:14 +0100

    Kod początkowy dla komponentu SummaryList wraz z css

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,39 @@
-import './App.css'
-import { Route, Routes } from 'react-router-dom';
-import { lazy } from 'react';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { lazy } from "react";
 
-const AuthPage = lazy(() => import('./pages/AuthPage/AuthPage'));
-const InputExpensesPage = lazy(() => import('./pages/InputExpensesPage/InputExpensesPage'));
-const InputIncomePage = lazy(() => import('./pages/InputIncomePage/InputIncomePage'));
-const ReportExpensesPage = lazy(() => import('./pages/ReportExpensesPage/ReportExpensesPage'));
-const ReportIncomePage = lazy(() => import('./pages/ReportIncomePage/ReportIncomePage'));
+const AuthPage = lazy(() => import("./pages/AuthPage/AuthPage"));
+const InputExpensesPage = lazy(() =>
+  import("./pages/InputExpensesPage/InputExpensesPage")
+);
+const InputIncomePage = lazy(() =>
+  import("./pages/InputIncomePage/InputIncomePage")
+);
+const ReportExpensesPage = lazy(() =>
+  import("./pages/ReportExpensesPage/ReportExpensesPage")
+);
+const ReportIncomePage = lazy(() =>
+  import("./pages/ReportIncomePage/ReportIncomePage")
+);
 
-function App() { 
+function App() {
   return (
     <>
       <Routes>
-        <Route path='/auth' element={<AuthPage />}/>
-        <Route path='/transaction/expenses' element={<InputExpensesPage />}/>
-        <Route path='/transaction/income' element={<InputIncomePage />} />
-        <Route path='/transaction/expense-categories' element={<ReportExpensesPage />} />
-        <Route path='/transaction/income-categories' element={<ReportIncomePage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/transaction/expenses" element={<InputExpensesPage />} />
+        <Route path="/transaction/income" element={<InputIncomePage />} />
+        <Route
+          path="/transaction/expense-categories"
+          element={<ReportExpensesPage />}
+        />
+        <Route
+          path="/transaction/income-categories"
+          element={<ReportIncomePage />}
+        />
       </Routes>
     </>
   );
-};
+}
 
-export default App
+export default App;
