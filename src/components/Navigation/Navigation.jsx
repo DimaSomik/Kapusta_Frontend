2899c258d1e26a2ecf11242393ba88c1f6a714f7
Dima Somik <ghubdmytrosomik@gmail.com>
2025-02-17 23:42:54 +0100

    fix

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import styles from "../Navigation/Navigation.module.css"; 
 import { useNavigate } from 'react-router-dom';
 
